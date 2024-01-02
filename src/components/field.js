c07c567433eaa5bead782ee6c407e1b7cfd8a250
Ralph Sommmer <ralphsommer@gmail.com>
2024-01-02 07:30:41 -0500

    translate app

---
+++ src/components/field.js
@@ -0,0 +1,19 @@
+import Flower from "./svg/flower";
+
+function Field({ value, onChange }) {
+  return (
+    <div className="field">
+      <Flower className="flower-right" fill="(110, 146, 119)" />
+      <Flower className="flower-left" fill="(249, 148, 59)" />
+      <h1>Translate App</h1>
+      <label>Enter English</label>
+      <input
+        className="input"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Field;
